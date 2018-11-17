f2448718b5d162069ab95952d6559c3d4ea9f7ea
Jesse Immonen <jesse.immonen@gmail.com>
2018-11-17 19:11:48 +0200

    Exercises 1-6

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+let persons = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456',
+        id: 1,
+    },
+    {
+        name: 'Martti Tienari',
+        number: '040-123456',
+        id: 2,
+    },
+    {
+        name: 'Arto Järvinen',
+        number: '040-123456',
+        id: 3,
+    },
+    {
+        name: 'Lea Kutvonen',
+        number: '040-123456',
+        id: 4,
+    },
+];
+
+function generateId() {
+    return Math.floor(Math.random() * Math.floor(100000));
+}
+
+function badRequest(res, errorText) {
+    return res.status(400).json({ error: errorText });
+}
+
+function findPerson({ id, name, number }) {
+    return persons.find(person => person.id === Number(id)
+            || person.name === name
+            || person.number === number);
+}
+
+const personsRouter = express.Router();
+
+personsRouter.get('/', (req, res) => {
+    res.json(persons);
+});
+
+personsRouter.get('/:id', ({ params: { id } }, res) => {
+    const person = findPerson({ id });
+    if (!person) res.status(404);
+    res.json(person);
+});
+
+personsRouter.post('/', ({ body }, res) => {
+    const person = body;
+
+    if (!person.name) return badRequest(res, 'name is missing');
+    if (!person.number) return badRequest(res, 'number is missing');
+    if (findPerson({ name: person.name })) return badRequest(res, 'name must be unique');
+
+    person.id = generateId();
+    persons.push(person);
+    return res.json(person);
+});
+
+personsRouter.delete('/:id', ({ params: { id } }, res) => {
+    persons = persons.filter(person => person.id !== Number(id));
+    res.status(204).end();
+});
+
+app.use('/api/persons', personsRouter);
+
+app.get('/info', (req, res) => {
+    const date = new Date().toISOString();
+    const infoString = `Puhelinluottelossa on ${persons.length} henkilön tiedot.<br> ${date}`;
+    res.send(infoString);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

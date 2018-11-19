8e1444633d2641f64773655b79ca8005c661e7a8
Jesse Immonen <jesse.immonen@gmail.com>
2018-11-19 03:09:09 +0200

    Add process.env.PORT handling

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.get('/info', (req, res) => {
     res.send(infoString);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
